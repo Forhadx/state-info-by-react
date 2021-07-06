fca5428a5e0941cec4607a746dd80f52c6a846c8
Forhad <forhadsh1@gmail.com>
2021-07-06 22:16:26 +0600

    add anmation and chang some design

--- src/StatePage/StatePage.js
+++ src/StatePage/StatePage.js
@@ -1,7 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import Details from "./components/Details";
-import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import axios from "axios";
 
 import "./StatePage.css";
 
@@ -10,29 +11,50 @@ const StatePage = () => {
   const [singleCountry, setSingleCountry] = useState("");
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  let sidebarClasses = ['sidebar'];
-  let mainClasses = ['main']
+  let sidebarClasses = ["sidebar"];
+
   if (sidebarOpen) {
-    sidebarClasses = ['sidebar', 'Open'];
-    mainClasses = ['main', 'Close']
+    sidebarClasses = ["sidebar", "Open"];
   }
 
-  const toggleButtonHandler = () =>{
-    setSidebarOpen(!sidebarOpen);
-  }
-
-  const filterCountryHandler = useCallback((country) => {
-    setSingleCountry(country[0]);
-    setCountryData(country);
+  useEffect(() => {
+    axios
+      .get("https://state-info-by-react-default-rtdb.firebaseio.com/items.json")
+      .then((res) => {
+        const fetchData = [];
+        for (let key in res.data) {
+          fetchData.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        const SortingData = [...fetchData].sort(function (a, b) {
+          var nameA = a.name.toUpperCase();
+          var nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        });
+        setSingleCountry(SortingData[0]);
+        setCountryData(SortingData);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-  const countryDataHandler = useCallback((country) => {
-    // this two lines scroll the page to top.
+  const toggleButtonHandler = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const countryDataHandler = (country) => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-
     setSingleCountry(country);
-  }, []);
+    setSidebarOpen(!sidebarOpen);
+  };
 
   return (
     <div className="state-page">
@@ -45,8 +67,7 @@ const StatePage = () => {
         <div className="state-info">STATE INFO</div>
       </header>
 
-      <aside className={sidebarClasses.join(' ')} >
-        <Search onLoadCountry={filterCountryHandler} />
+      <aside className={sidebarClasses.join(" ")}>
         <div className="all-C-name">
           {courtryData
             ? courtryData.map((data) => (
@@ -62,13 +83,16 @@ const StatePage = () => {
         </div>
       </aside>
 
-      <main className={mainClasses.join(' ')}>
+      <main className="main">
         {singleCountry ? (
           <Details singleCountryData={singleCountry} />
         ) : (
           <Spinner />
         )}
-        <div className="admin">Go to admin Panel http://localhost:3000/admin (turn off Add data)</div>
+        <div className="admin">
+          Go to admin Panel https://unruffled-hugle-819064.netlify.app/admin
+          (turn off Add data)
+        </div>
       </main>
     </div>
   );
--- src/StatePage/components/Details.js
+++ src/StatePage/components/Details.js
@@ -1,63 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const Details = React.memo(props => {
+const Details = React.memo((props) => {
   const c = props.singleCountryData;
-
-  //console.log('data: ',props.singleCountryData.religions)
-  
-  //const arr = [...props.singleCountryData.religions];
-
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  }, []);
 
   return (
-    <React.Fragment>
+    <div className="main_details">
       <div className="coutry-name">
-        <div className="name">
+        <div className="name" data-aos="slide-right">
           <h1>{c.name}</h1>
         </div>
-        <img src={c.flag} alt={c.name} />
+        <img src={c.flag} alt={c.name} data-aos="slide-left" />
       </div>
-      <div className="description">{c.description}</div>
+      <div className="description" data-aos="zoom-out-up" data-aos-delay="1500">
+        {c.description}
+      </div>
       <div className="details">
-        <div className="detail-row">
+        <div className="detail-row" data-aos="slide-up">
           <h2>Capital</h2>
           <h2>{c.capital}</h2>
         </div>
-        <div className="detail-row">
+        <div className="detail-row" data-aos="slide-up">
           <h2>Language</h2>
           <h2>{c.language}</h2>
-          
         </div>
-        <div className="religion-row">
+        <div className="religion-row" data-aos="slide-up">
           <h2>Religion</h2>
           <div>
-          {
-            c.religions ? c.religions.map(p =>(
-              <h2 key={p}>{p}</h2>
-            )) : null
-          }
+            {c.religions ? c.religions.map((p) => <h2 key={p}>{p}</h2>) : null}
           </div>
         </div>
-        <div className="detail-row">
+        <div className="detail-row" data-aos="slide-up">
           <h2>Area</h2>
           <h2>{c.area}</h2>
         </div>
-        <div className="detail-row">
+        <div className="detail-row" data-aos="slide-up">
           <h2>Currency</h2>
           <h2>{c.currency}</h2>
         </div>
       </div>
       <div className="images">
-        {
-          c.images ? c.images.map(i =>(
-            <img key={i} src={i} alt={c.name} />
-          )) : null
-        }
+        {c.images
+          ? c.images.map((i) => (
+              <img
+                key={i}
+                src={i}
+                alt={c.name}
+                data-aos="zoom-in"
+                data-aos-delay="800"
+              />
+            ))
+          : null}
       </div>
 
-      <div className="map">
-        <iframe title={c.name + ' map'} src={c.map}></iframe>
+      <div className="map" data-aos="flip-down" data-aos-delay="800">
+        <iframe title={c.name + " map"} src={c.map}></iframe>
       </div>
-    </React.Fragment>
+    </div>
   );
 });
 
